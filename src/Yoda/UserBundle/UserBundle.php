6c73bf34a9e5466b593fc6e4f045c88222b6ed87
bellaajhabib <bellaaj.habib@gmail.com>
2016-07-03 14:35:29 +0100

    security auth

---
+++ src/Yoda/UserBundle/UserBundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: habib
+ * Date: 7/3/2016
+ * Time: 12:59 PM
+ */
+
+namespace Yoda\UserBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle  extends Bundle{
+
+} 
